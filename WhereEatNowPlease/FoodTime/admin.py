76782ab16aeac173983de1535e0455766c141123
Jared <gookaney@gmail.com>
2023-01-12 15:02:19 -0700

    Updated settings, added admin.py

--- WhereEatNowPlease/FoodTime/admin.py
+++ WhereEatNowPlease/FoodTime/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Account, FoodCatagory, Restaurant
 
 # Register your models here.
+admin.register(Account)
+admin.register(FoodCatagory)
+admin.register(Restaurant)
--- WhereEatNowPlease/WhereEatNowPlease/settings.py
+++ WhereEatNowPlease/WhereEatNowPlease/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FoodTime.apps.FoodtimeConfig',
+    'FoodTime',
 ]
 
 MIDDLEWARE = [
