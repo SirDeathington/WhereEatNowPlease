d56ed71f17e418b924311d2f87e97eb86fb84488
DJ Gillespie <dj@monumetric.com>
2023-01-14 01:53:15 -0700

    Fixed models and admin

--- WhereEatNowPlease/FoodTime/admin.py
+++ WhereEatNowPlease/FoodTime/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Account, FoodCatagory, Restaurant
 
 # Register your models here.
-admin.register(Account)
-admin.register(FoodCatagory)
-admin.register(Restaurant)
+admin.site.register(Account)
+admin.site.register(FoodCatagory)
+admin.site.register(Restaurant)
